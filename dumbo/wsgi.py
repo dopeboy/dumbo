1aa2e354e3b568b3ad4ba902a53327f6a8be282a
Manish Sinha <arithmetic@gmail.com>
2016-01-14 21:03:04 -0800

    heroku

--- dumbo/wsgi.py
+++ dumbo/wsgi.py
@@ -3,5 +3,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "leaderboard.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dumbo.settings")
 application = Cling(get_wsgi_application())
