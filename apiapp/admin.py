7f95a8daa4fb743aa5c933adf16fa783d6a42ff5
Manish Sinha <arithmetic@gmail.com>
2016-04-23 14:05:30 -0700

    show all on admin page

--- apiapp/admin.py
+++ apiapp/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from apiapp import models
 
+
+class FooAdmin(admin.ModelAdmin):
+    list_max_show_all = 1000
+    list_per_page = 1000
+
 # Register your models here.
 admin.site.register(models.Exam)
 admin.site.register(models.Problem)
