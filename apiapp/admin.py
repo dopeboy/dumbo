47bd96a30a48f19059128a86f4e3fbcb27a34ab7
Manish Sinha <arithmetic@gmail.com>
2016-04-23 14:09:57 -0700

    admin pagination disable

--- apiapp/admin.py
+++ apiapp/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from apiapp import models
 
 
-class FooAdmin(admin.ModelAdmin):
+class StepAdmin(admin.ModelAdmin):
     list_max_show_all = 1000
     list_per_page = 1000
 
 # Register your models here.
 admin.site.register(models.Exam)
 admin.site.register(models.Problem)
-admin.site.register(models.Step)
+admin.site.register(models.Step, StepAdmin)
 admin.site.register(models.Event)
